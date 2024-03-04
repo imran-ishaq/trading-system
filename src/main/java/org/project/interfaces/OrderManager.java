8e07683f6b204c77bfe57cb320617d08d31808b4
ImranIshaq <imranishaq024@gmail.com>
2024-03-04 18:38:17 +0500

    changed return type from String to void for add order

--- src/main/java/org/project/interfaces/OrderManager.java
+++ src/main/java/org/project/interfaces/OrderManager.java
@@ -7,7 +7,7 @@ import org.project.exceptions.OrderException;
 import java.util.List;
 
 public interface OrderManager {
-    String addOrder(Order order) throws OrderException;
+    void addOrder(Order order) throws OrderException;
     void cancelOrder(String orderId) throws OrderException;
     List<Order> getOrders(String instrumentId);
     List<Order> getOrders(String instrumentId, OrderType orderType);
