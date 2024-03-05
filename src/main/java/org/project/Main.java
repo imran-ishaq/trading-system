f593468ba0e20fe41f18c8e618f4d4ca4fdb0d20
ImranIshaq <imranishaq024@gmail.com>
2024-03-05 19:48:30 +0500

    Removed uncleaned code

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -10,7 +10,6 @@ import org.project.utils.Instrument;
 import org.project.utils.InstrumentComponent;
 import org.project.utils.Order;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -49,52 +48,32 @@ public class Main {
 
 
         // Create some dummy orders
-//        Order buyOrder1 = new Order("1", "Trader1", OrderType.BUY, stock1, 100, 155.0);
-//        Order sellOrder1 = new Order("2", "Trader2", OrderType.SELL, stock1, 100, 155.0);
+        Order buyOrder1 = new Order("1", "Trader1", OrderType.BUY, stock1, 100.0, 155.0);
+        Order sellOrder1 = new Order("2", "Trader2", OrderType.SELL, stock1, 100.0, 155.0);
         Order buyOrder2 = new Order("3", "Trader3", OrderType.BUY, basket, 200.0, null);
         Order sellOrder2 = new Order("4", "Trader4", OrderType.SELL, basket, 100.0, null);
-//        Order buyOrder3 = new Order("5", "Trader2", OrderType.BUY, stock2, 50, 155.0);
+        Order buyOrder3 = new Order("5", "Trader2", OrderType.BUY, stock2, 50.0, 155.0);
 
         // Add orders to the order manager
-//        try {
-//            orderManager.addOrder(buyOrder1);
-//        } catch (OrderException e) {
-//            LOGGER.log(Level.SEVERE,"Error adding orders: " + e.getMessage());
-//        }
-//        try {
-//            orderManager.addOrder(sellOrder1);
-//        } catch (OrderException e) {
-//            LOGGER.log(Level.SEVERE,"Error adding orders: " + e.getMessage());
-//        }
-        try {
-            orderManager.addOrder(buyOrder2);
-        } catch (OrderException e) {
-            LOGGER.log(Level.SEVERE,"Error adding orders: " + e.getMessage());
-        }
-        try {
-            orderManager.addOrder(sellOrder2);
-        } catch (OrderException e) {
-            LOGGER.log(Level.SEVERE,"Error adding orders: " + e.getMessage());
-        }
-//        try {
-//            orderManager.addOrder(buyOrder3);
-//        } catch (OrderException e) {
-//            LOGGER.log(Level.SEVERE,"Error adding orders: " + e.getMessage());
-//        }
+        makeOrders(orderManager, buyOrder1);
+        makeOrders(orderManager, buyOrder2);
+        makeOrders(orderManager, sellOrder1);
+        makeOrders(orderManager, sellOrder2);
+        makeOrders(orderManager, buyOrder3);
 
         // Match orders
         tradingEngine.matchOrders("1");
         tradingEngine.matchOrders("3");
         tradingEngine.matchOrders("2");
         // Print remaining orders
-        System.out.println("Remaining orders:");
-        List<Order> remainingOrders = new ArrayList<>();
-//        remainingOrders.addAll(orderManager.getOrders("1"));
-        remainingOrders.addAll(orderManager.getOrders("3"));
-//        remainingOrders.addAll(orderManager.getOrders("2"));
-        for (Order order : remainingOrders) {
-            System.out.println(order.getId() + " - " + order.getTraderId() + " - " + order.getType() +
-                    " - " + order.getQuantity() + " - " + order.getPrice() + " - " + order.getStatus());
+        LOGGER.log(Level.SEVERE, "Remaining orders:");
+    }
+
+    private static void makeOrders(InMemoryOrderManager orderManager, Order order) {
+        try {
+            orderManager.addOrder(order);
+        } catch (OrderException e) {
+            LOGGER.log(Level.SEVERE,String.format("Error adding orders: %s", e.getMessage()));
         }
     }
 }
